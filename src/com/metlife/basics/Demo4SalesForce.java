c57d24c7c480f76da4309a54c04f0d6042a310e6
balaji-githubstore <balajidinakaran1@gmail.com>
2024-01-10 15:35:35 +0530

    XPATH

---
+++ src/com/metlife/basics/Demo4SalesForce.java
@@ -0,0 +1,24 @@
+package com.metlife.basics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Demo4SalesForce {
+    public static void main(String[] args) {
+//        WebDriver driver=new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+//
+//        driver.get("https://login.salesforce.com/?locale=in");
+        //click try for free
+        //enter firstname as john
+        //enter emailaddress as john@CCCS.COM
+        //select jobtitle as "IT Manager"
+        //select employees to '101-200 emplpoyees'
+        //click on checkbox of agreement
+
+        System.out.println("");
+    }
+}
