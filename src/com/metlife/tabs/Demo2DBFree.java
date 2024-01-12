e981b39ef866c4c03d7fefef319066c0f42dbf47
balaji-githubstore <balajidinakaran1@gmail.com>
2024-01-12 17:16:01 +0530

    tabs

--- src/com/metlife/tabs/Demo2DBFree.java
+++ src/com/metlife/tabs/Demo2DBFree.java
@@ -16,6 +16,7 @@ public class Demo2DBFree {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
+
         driver.get("https://www.db4free.net/");
 
         //click on phpMyAdmin »
